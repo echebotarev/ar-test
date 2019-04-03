6ac2c379f64ee7be6da95f541a48baa3b524b582
Egor Chebotarev <eg.chebotarev@gmail.com>
2019-04-03 19:00:00 +0300

    Остановка/запуск видео при lost/found

--- marker-found-lost/js/runVideo.js
+++ marker-found-lost/js/runVideo.js
@@ -1,8 +1,12 @@
+var Played = false;
+
 (function () {
     var btn = document.getElementById('button');
     btn.addEventListener('click', function () {
     	var video = document.getElementById('video');
         video.play();
         video.muted = false;
+
+        Played = true;
     })
 })();
