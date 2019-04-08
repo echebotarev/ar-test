54e5f8bc6d52cc011971cbdaa1521b77b1421bc5
Egor Chebotarev <eg.chebotarev@gmail.com>
2019-04-08 17:46:01 +0300

    Тест hiro

--- marker-found-lost/js/runVideo.js
+++ marker-found-lost/js/runVideo.js
@@ -3,7 +3,7 @@ var Played = false;
 (function () {
     var btn = document.getElementById('button');
     btn.addEventListener('click', function () {
-    	var video = document.getElementById('cartoon');
+    	var video = document.getElementById('animal');
         video.play();
         video.muted = false;
 
